15ad477edc9f10945692ff6522fe688430a55d29
Anuj Karn <148365884+Anujkarn1542@users.noreply.github.com>
2026-09-20 17:05:57 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,41 @@
+class MinStack {
+public:
+stack<pair<int,int>>st;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(st.empty()){
+            st.push({val,val});
+        }else{
+            st.push({val,min(val,st.top().second)});
+        }
+    }
+    
+    void pop() {
+        if(st.empty()){
+            return;
+        }else{
+            st.pop();
+        }
+    }
+    
+    int top() {
+        if(st.empty()){
+            return -1;
+        }else{
+            return st.top().first;
+        }
+        
+    }
+    
+    int getMin() {
+        if(st.empty()){
+            return -1;
+        }
+        else{
+            return st.top().second;
+        }
+    }
+};
